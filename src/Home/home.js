9b95ad7c1b2779684465dc1060ddd868aff7fd3c
VillalvaLijo <samuelvillalvalijo@gmail.com>
2021-01-15 17:05:07 -0600

    Update

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,1 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+    render(){
+        return(
+            <body>
+                <h1>THE REALTOR PAGE</h1>
+            </body>
+        )
+    }
+}
+
+export default Home;
